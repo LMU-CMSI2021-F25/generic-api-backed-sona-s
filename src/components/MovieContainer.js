eae34631e8ec8a1100e776ee459bb48aeed684c1
Sona <sargsyan.sona1.y@gmail.com>
2025-09-29 23:19:55 -0700

    fetching data, and displaying some information with UI

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import Result from "./Result";
 
 function MovieContainer() {
+  const [searchValue, setSearchValue] = useState("");
+  const [movieData, setMovieData] = useState(null);
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    console.log("API Key:", process.env.REACT_APP_API_KEY);
+    console.log("Searching for:", searchValue);
+    fetchMovieData(searchValue);
+    console.log("Movie Data:", movieData);
+  };
+
+  const fetchMovieData = async (title) => {
+    try {
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`
+      );
+      if (!response.ok) {
+        console.error("HTTP error:", response.status);
+        return null;
+      }
+
+      const data = await response.json();
+
+      if (data.Response === "False") {
+        throw new Error(data.Error || "Movie not found");
+      }
+      setMovieData(data);
+      return data;
+    } catch (error) {
+      console.error("Fetch error:", error);
+      return null;
+    }
+  };
+
   return (
     <MovieContainerComponent>
       <h1>Book-to-Movie Tracker</h1>
       <InputContainer>
-        <Input type="text" placeholder="Search for a book title..." />
-        <Button>Search</Button>
+        <Input
+          value={searchValue}
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="Search for a book title..."
+        />
+        <Button onClick={handleSearchClick}>Search</Button>
       </InputContainer>
+
+      {movieData ? (
+        <Result movieData={movieData} />
+      ) : (
+        <ResultContainer>
+          <p>Start searching movies...</p>
+        </ResultContainer>
+      )}
     </MovieContainerComponent>
   );
 }
 
 const MovieContainerComponent = styled.div`
-padding:20px;
+  padding: 20px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -55,6 +106,14 @@ const Button = styled.button`
   &:hover {
     background-color: #0056b3;
   }
+`;
+
+const ResultContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  color:gray;
 `;
 
 export default MovieContainer;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-function Result() {
+function Result({ movieData }) {
+  const calculateStars = (rating) => {
+    const stars = Math.round((rating / 10) * 10);
+    return Array.from({ length: 10 }, (_, index) =>
+      index < stars ? "★" : "☆"
+    ).join("");
+  };
+
+  const getActors = (actors) => {
+    return actors.split(",").map((actor) => actor.trim());
+  };
+
   return (
-    <div>Result</div>
-  )
+    <ResultContainer>
+      <MainInfo>
+        <div>
+          <Image src={movieData.Poster}></Image>
+        </div>
+        <div>
+          <h2>{movieData.Title}</h2>
+          <p>{movieData.Director}</p>
+          <p>{movieData.Genre}</p>
+          <p>Year: {movieData.Year}</p>
+          <div style={{ marginTop: "10px", color: "yellow", fontSize: "20px" }}>
+            <p style={{ color: "gold", marginRight: "5px" }}>
+              {calculateStars(movieData.imdbRating)}{" "}
+              <span style={{ color: "gray" }}>{movieData.imdbRating} IMDb</span>
+            </p>
+          </div>
+        </div>
+      </MainInfo>
+      <SecondaryInfo>
+        <Actors>
+          {getActors(movieData.Actors).map((actor, index) => (
+            <Actor key={index}>{actor}</Actor>
+          ))}
+        </Actors>
+        <p>{movieData.Plot}</p>
+      </SecondaryInfo>
+    </ResultContainer>
+  );
 }
 
-export default Result
+const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 100%;
+`;
+
+const MainInfo = styled.div`
+  display: flex;
+  gap: 20px;
+  margin: 20px;
+`;
+
+const SecondaryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 20px;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  border-radius: 5px;
+`;
+
+const Actors = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-bottom: 10px;
+`;
+
+const Actor = styled.p`
+  background-color: #f0f0f0;
+  padding: 5px 15px;
+  border: 1px solid #ccc;
+  border-radius: 100px;
+  font-size: 12px;
+`;
+
+export default Result;
